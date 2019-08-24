1c9dfdfe21b65d1785276180d841e242c50303f1
emadpres <emadpres@gmail.com>
2019-08-24 23:30:09 +0200

    Basic GLEW setup

--- OpenGL/src/App.cpp
+++ OpenGL/src/App.cpp
@@ -1,5 +1,5 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
 
 int main(void)
@@ -20,6 +20,19 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glewInit();
+
+
+
+	// Testing OpenGL methods
+	std::cout << glGetString(GL_VERSION) << std::endl;
+	unsigned int a;
+	glGenBuffers(1, &a);
+
+
+
+
+
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -28,11 +41,7 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
